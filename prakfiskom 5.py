1d1435c639d3ca862eaa0a38b5c34664eb86066b
nurilcmla <114709709+nurilcmla@users.noreply.github.com>
2022-11-03 12:18:58 +0700

    Add files via upload

---
+++ prakfiskom 5.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+to = 0
+tn = 300
+ndata = 1000
+
+t = np.linspace(to, tn, ndata)
+h = t[2]-t[1]
+
+N = 1000 #rakyat
+I0 = 1
+R0 = 0
+S0 = N - I0 - R0
+
+I = np.zeros(ndata)
+S = np.zeros(ndata)
+R = np.zeros(ndata)
+
+I[0] = I0
+R[0] = R0
+S[0] = S0
+
+beta = 0.2
+gamma = 0.1
+
+for i in range (0, ndata-1):
+    S[i+1] = S[i] - h*beta/N*S[i]*I[i]
+    I[i+1] = I[i] + h*beta/N*S[i]*I[i] - h*gamma*I[i]
+    R[i+1] = R[i] + h*gamma*I[i]
+
+plt.plot(t,S,label = 'S')
+plt.plot(t,I,label = 'I')
+plt.plot(t,R,label = 'R')
+
+plt.legend()
+plt.show()
